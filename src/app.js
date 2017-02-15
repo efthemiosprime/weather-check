edcce4424d4b261f11039bf2198c65d7d7337dfd
efthemios suyat <efthemios.suyat@gmail.com>
2017-02-14 22:05:47 -0500

    setup api request

--- src/app.js
+++ src/app.js
@@ -1,4 +1,25 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS Boiler Running...');
+const rxFetch = require("rxjs-fetch");
+const searchWeather = document.getElementById("searchWeather");
+const searchStream = Rx.Observable.fromEvent(searchWeather, "submit")
+.map(e => {
+    e.preventDefault();
+    return {
+        city: e.currentTarget[0].value.replace(' ', '_'),
+        state: e.currentTarget[1].value
+    };
+})
+.switchMap( v => getWeather(v.city, v.state))
+.map(v => v.current_observation);
+
+searchStream.subscribe(res => {
+    console.log(res);
+})
+
+
+
+function getWeather(city, state) {
+     return rxFetch(`http://api.wunderground.com/api/b6381e12550334c7/conditions/q/${state}/${city}.json`).json();
+}
